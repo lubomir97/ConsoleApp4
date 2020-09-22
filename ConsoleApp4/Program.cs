68583cc0fbfd86ca27b0fd06aa563cef9995c76f
Serge <sanichace@gmail.com>
2020-09-22 13:06:10 +0300

    merging

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp4
 {
     class Program
     {
+        class MyClass
+        {
+            string name;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
